d762c20ff07241aa297010291b2e16c86a12d936
carogab99 <toledoc509@gmail.com>
2020-02-04 07:50:00 -0500

    ok

--- Estudiante/src/estudiante/clases/Matricula.java
+++ Estudiante/src/estudiante/clases/Matricula.java
@@ -3,19 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estudiante;
+package estudiante.clases;
+
+import java.util.List;
 
 /**
  *
  * @author CARITO
  */
-public class Estudiante {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Matricula {
     
 }
